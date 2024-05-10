e711e203dd536c1943120db47c3b50feacd1e3cf
Oltion-Koceku <koceku17@gmail.com>
2024-05-10 14:51:48 +0200

    [first]

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('home');
+
+        $movie = Movie::all();
+
+        dd($movie);
+
+
+        return view('home', compact('movie'));
     }
+
+
 
     public function nuovaPagina(){
         return view('nuova-pagina');
